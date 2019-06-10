a2dc4f68d32fd2ccb1b1d84573a403adde7164dd
Dagim Sisay <dagiopia@gmail.com>
2019-06-10 15:34:35 +0300

    scheme api update

--- api/scheme/sumo.scm
+++ api/scheme/sumo.scm
@@ -2,9 +2,7 @@
     #:export
         (reset-sumo-server
         set-sumo-server
-        sumo-superclasses
-        sumo-subclasses
-        sumo-related))
+        sumo-search))
 
 
 (use-modules (opencog)
@@ -12,6 +10,7 @@
              (opencog query))
 
 (use-modules (web client)
+             (ice-9 regex)
              (json))
 
 (define default-sumo-server "http://144.76.153.5:7084")
@@ -28,11 +27,19 @@
 
 (define (set-sumo-server addr)
 "
-    set-sumo-server addr - set the current sumo server 
+    set-sumo-server addr - set the current sumo server
                            address to addr
                            http://<address>:<port>
 "
     (set! sumo-server addr))
+
+(define (load-ontologies onts)
+    (get-http-response
+        (string-append
+            sumo-server
+            "/loadontologies/"
+            onts)))
+
 
 (define (get-http-response req)
     (call-with-values
@@ -40,51 +47,58 @@
         (lambda (r-head r-body) r-body)))
 
 
-(define (fetch-wrap query-type query)
+(define (fetch-wrap relation query query-node)
     (define resp
         (get-http-response
             (string-append
                 sumo-server
-                query-type
                 query)))
+    (define (resp-atom nod)
+        (EvaluationLink
+            (PredicateNode relation)
+            (ListLink
+                nod
+                query-node)))
     (if (not (string-ci=? "" resp))
-    (map ConceptNode
+    (map resp-atom (map ConceptNode
         (map cdr
             (cdar
-                (json-string->scm resp)))) '()))
+                (json-string->scm resp))))) '()))
 
-(define (sumo-superclasses nod)
+(define search-options (list "subclasses" "superclasses" "related"))
+
+(define* (sumo-search
+            nod
+            search-type
+            #:key
+                (depth 0)
+                (whitelist "")
+                (blacklist ""))
 "
-    sumo-superclasses nod - query the sumo-server for
-                            superclasses of nod.
-                            nod should be an opencog
-                            node with a valid name.
-    Return: list of superclass Nodes reachable from nod
+    sumo-search - query the sumo-server for either
+                  superclasses, subclasses of or related to nod.
+          (fixed)
+            nod         - nod should be an opencog
+                          ConceptNode with a valid name.
+            search-type - either of <superclasses> <subclasses> or <related>
+          (optionals)
+            depth       - depth of recursion
+            whitelist   - comma delimited list of
+                          SUMO categories to search
+            blacklist   - comma delimited list of
+                          SUMO categories to avoid search
+
+    Return: atomese of Nodes reachable from nod with
+            relationship-type as predicate
 "
-    (if (cog-atom? nod)
-        (fetch-wrap "/superclasses/" (cog-name nod))
+    (if (and (cog-atom? nod) (member search-type search-options))
+            (let ((req-c (string-append "/" search-type "/" (cog-name nod)
+                                         "/depth/" (number->string depth))))
+            (if (not (equal? whitelist ""))
+                (set! req-c (string-append req-c "/whitelist/" whitelist)))
+            (if (not (equal? blacklist ""))
+                (set! req-c (string-append req-c "/blacklist/" blacklist)))
+            (fetch-wrap (string-capitalize search-type) req-c nod)
+            )
         '()))
 
-(define (sumo-subclasses nod)
-"
-    sumo-subclasses nod - query the sumo-server for
-                            subclasses of nod.
-                            nod should be an opencog
-                            node with a valid name.
-    Return: list of subclass Nodes reachable from nod
-"
-    (if (cog-atom? nod)
-        (fetch-wrap "/subclasses/" (cog-name nod))
-        '()))
-
-(define (sumo-related depth nod)
-    (if (cog-atom? nod)
-        (fetch-wrap
-            (string-append
-                sumo-server
-                "/related/"
-                (number->string depth)
-                "/" (cog-name nod))
-            "related"
-            nod)
-        '()))
